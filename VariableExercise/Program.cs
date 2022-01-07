5ab0daa06331f95b64d4d98ed9b2b23f77dc8e16
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-07 15:50:42 -0600

    initial commit

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string dogName; // declaration syntax
+            dogName = "Mila"; //initialized
+
+            int dogAge = 2; // declaring & initializing 
+
+            char firstInitial = 'R';
+
+            bool isHungry = true;
+
+            double dogWeight = 50.5;
+
+            decimal dogHeight = 24.7m;
+
+            Console.WriteLine($"My dog's name is {dogName}. She is {dogAge} years old and her initialized.");
+            Console.WriteLine($"He weights {dogWeight} pounds and he is {dogHeight}\" tall.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+} 
